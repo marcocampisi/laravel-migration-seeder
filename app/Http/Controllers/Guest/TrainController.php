0622b614d2772b8bb907cf23502075830c88d786
marcocampisi <marco.campisi@outlook.it>
2023-09-12 16:56:28 +0200

    Add controller index method

--- app/Http/Controllers/Guest/TrainController.php
+++ app/Http/Controllers/Guest/TrainController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use App\Models\Train;
 
 class TrainController extends Controller
 {
-    //
+    public function index() 
+    {
+        $currentDate = Carbon::now();
+        $trains = Train::whereDate('orario_partenza', '>=', $currentDate)->get();
+
+        return view('trains.index', compact('trains'));
+    }
 }

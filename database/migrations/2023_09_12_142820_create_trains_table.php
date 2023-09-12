819731925d0fb2cb864335d1f1ae1f83e7bd242b
marcocampisi <marco.campisi@outlook.it>
2023-09-12 16:30:43 +0200

    Create trains table

---
+++ database/migrations/2023_09_12_142820_create_trains_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('azienda', 100);
+            $table->string('stazione_partenza', 100);
+            $table->string('stazione_arrivo', 100);
+            $table->dateTime('orario_partenza');
+            $table->dateTime('orario_arrivo');
+            $table->string('codice_treno', 10);
+            $table->integer('numero_carrozze');
+            $table->boolean('in_orario', 1);
+            $table->boolean('cancellato', 1);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
